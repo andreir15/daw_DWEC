5a9a664c9f73c51acced9f0265456062e52e0869
andreir15 <90853700+andreir15@users.noreply.github.com>
2022-01-28 12:31:50 +0100

    Delete area.ts

--- typescript/area.ts
+++
@@ -1,5 +0,0 @@
-function area(shape:string, width:number, height:number){
-var area=width*height;
-return "Soy un "+shape+" con un area de "+area+" cm cuadrados";
-}
-document.body.innerHTML=area("rectangulo",30,15);
